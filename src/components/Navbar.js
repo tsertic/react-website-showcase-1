dbabd39a8252fdea9381fdfe79aca56f199de28c
unknown <tsertic5@gmail.com>
2021-03-21 13:44:47 +0100

    updated navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  const [click, setClick] = useState(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon">
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
